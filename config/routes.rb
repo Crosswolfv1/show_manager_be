1bb696a19565991696261fda8cfac3f58e357ba1
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-21 13:03:23 -0600

    modified route paramenters for better reading

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
-      resources :festivals, only: [:index, :show], param: :festival_id do
-        delete '/artist/:artist_id', to: 'attending_artist_festials#delete', as: :delete_attending_artist_festival
+      resources :festivals, only: [:index, :show], param: :id do
+        delete '/artist/:artist_id', to: 'attending_artist_festivals#delete', as: :delete_attending_artist_festival
       end
     end
   end

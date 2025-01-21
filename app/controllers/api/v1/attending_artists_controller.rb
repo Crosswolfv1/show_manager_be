c3dd208cff81d242974426d23169b0e5809920ef
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-21 12:51:21 -0600

    added error handleing

--- app/controllers/api/v1/attending_artists_controller.rb
+++ app/controllers/api/v1/attending_artists_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::AttendingArtistsController < ApplicationController
+  rescue_from ArgumentError, with: :invalid_parameters
+
   def index
     @artists = AttendingArtist.all
   end
@@ -11,4 +13,7 @@ class Api::V1::AttendingArtistsController < ApplicationController
 
   private
 
+  def invalid_parameters(exception)
+    render json: ErrorSerializer.format_error(ErrorMessage.new(exception, 404)), status: :not_found
+  end
 end

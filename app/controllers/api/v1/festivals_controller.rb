f096883d8af1fcd7e7343b46dda8110d454a73b9
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-21 12:51:07 -0600

    created festival crud actions

--- app/controllers/api/v1/festivals_controller.rb
+++ app/controllers/api/v1/festivals_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::FestivalsController < ApplicationController
+  rescue_from ArgumentError, with: :invalid_parameters
+
   def index
-    @festivals = Festival.all
+    festivals = Festival.all
+    options = {}
+    options[:meta] = {total: festivals.count}
+    render json: FestivalSerializer.new(festivals, options)
   end
 
   def show
+    festival = Festival.find_by(id: params[:id])
+    render json: FestivalSerializer.new(festival)
   end
 
   def create
@@ -13,4 +20,7 @@ class Api::V1::FestivalsController < ApplicationController
 
   private
 
+  def invalid_parameters(exception)
+    render json: ErrorSerializer.format_error(ErrorMessage.new(exception, 404)), status: :not_found
+  end
 end

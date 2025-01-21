98eb308697ec3b16d4bc20252efc808e4bae14f9
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-21 15:34:41 -0600

    added has many relationship

--- app/serializers/festival_serializer.rb
+++ app/serializers/festival_serializer.rb
@@ -5,7 +5,9 @@ class FestivalSerializer
   set_id :id
   attributes :name, :location, :start_time, :end_time
 
-  private
+  has_many :attending_artists, serializer: AttendingArtistSerializer
+  
+private
 
   attribute :start_time do |attr|
     attr.start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -14,4 +16,5 @@ class FestivalSerializer
   attribute :end_time do |attr|
     attr.end_time.strftime("%Y-%m-%dT%H:%M:%SZ")
   end
+ 
 end

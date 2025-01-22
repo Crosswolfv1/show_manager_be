3dc929ff68fa0c887a9bf78a4e6f14aedf0d7302
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-22 11:50:39 -0600

    added param validation and error handling

--- app/controllers/api/v1/attending_artist_festivals_controller.rb
+++ app/controllers/api/v1/attending_artist_festivals_controller.rb
@@ -1,17 +1,25 @@
 class Api::V1::AttendingArtistFestivalsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_parameters
   rescue_from ArgumentError, with: :invalid_parameters
 
   def delete
+    validate_params
     entry = AttendingArtistFestival.find_by(
       festival_id: params[:festival_id], 
       attending_artist_id: params[:artist_id]
     )
+    raise ArgumentError, "Artist not found at this festival" unless entry
     AttendingArtistFestival.delete(entry.id)
   end
 
   private
   
+  def validate_params
+    Festival.find(params[:festival_id]) 
+    AttendingArtist.find(params[:artist_id]) 
+  end
+
   def invalid_parameters(exception)
-    render json: ErrorSerializer.format_error(ErrorMessage.new(exception, 404)), status: :not_found
+    render json: ErrorSerializer.format_error(exception, 404), status: :not_found
   end
 end
